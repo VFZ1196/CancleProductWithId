bbef907f37540c2252d1118926b92e4dc0db88a9
arjun <arjunyadav102030@gmail.com>
2021-02-19 17:57:42 +0530

    first commit

--- orderWithId-and-CancleWithId/src/main/java/com/ProductOrderId/controller/ShoppingController.java
+++ orderWithId-and-CancleWithId/src/main/java/com/ProductOrderId/controller/ShoppingController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -35,6 +36,7 @@ public class ShoppingController {
 	}
 
 	@GetMapping("/shopping/{id}")
+	@Transactional(timeout = 20)
 	public Shopping getOne(@PathVariable int id) {
 		return shoppingRepository.findById(id).get();
 	}
